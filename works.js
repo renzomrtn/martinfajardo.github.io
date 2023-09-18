5ff64aa4e2af087165079ca15885e280194e8e28
renzomrtn <martinlorenz.fajardo@gmail.com>
2023-09-18 20:06:31 +0800

    js separation

---
+++ works.js
@@ -0,0 +1,13 @@
+    // Open the modal and display the clicked image
+    function openModal(img) {
+        var modal = document.getElementById("myModal");
+        var modalImg = document.getElementById("modalImage");
+        modal.style.display = "flex";
+        modal.classList.add("show"); // Add the "show" class to activate the transition
+        modalImg.src = img.src;
+      }
+      // Close the modal
+      function closeModal() {
+        var modal = document.getElementById("myModal");
+        modal.style.display = "none";
+      }
