25bea7a5944d11f427e25b1bc2043c845b15ffe1
renzomrtn <martinlorenz.fajardo@gmail.com>
2023-09-21 18:14:29 +0800

    added notification (site under dev.)

---
+++ index.js
@@ -0,0 +1,8 @@
+function closeNotification() {
+    var notification = document.getElementById("notification");
+    notification.style.display = "none";
+}
+
+// Show the notification initially (optional)
+var notification = document.getElementById("notification");
+notification.style.display = "block";
