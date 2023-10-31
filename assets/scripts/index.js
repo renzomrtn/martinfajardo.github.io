a48720196341cb01e1ecbe941a33dcbeeceb1e7c
renzomrtn <martinlorenz.fajardo@gmail.com>
2023-10-31 12:25:18 +0800

    sidebar improvements

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,21 +6,3 @@ function closeNotification() {
 // Show the notification initially
 var notification = document.getElementById("notification");
 notification.style.display = "block";
-
-// Get the header element
-const header = document.querySelector('.top-nav');
-
-// Get the initial position of the header
-const headerPosition = header.offsetTop;
-
-// Function to check if the header should become sticky
-function makeHeaderSticky() {
-    if (window.pageYOffset > headerPosition) {
-        header.classList.add('sticky');
-    } else {
-        header.classList.remove('sticky');
-    }
-}
-
-// Add a scroll event listener to trigger the function
-window.addEventListener('scroll', makeHeaderSticky);
