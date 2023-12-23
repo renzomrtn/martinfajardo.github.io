dc7e8da44e00276cd80c6f1e7bc0523260cec4f8
renzomrtn <martinlorenz.fajardo@gmail.com>
2023-12-23 19:57:44 +0800

    finished the toggle

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,4 +9,11 @@ notification.style.display = "block";
 
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
-  }
+    
+    // Get the <i> element inside the .toggle element
+    const toggleIcon = document.querySelector('.toggle i');
+
+    // Toggle the class for the icon
+    toggleIcon.classList.toggle('fa-toggle-on');
+    toggleIcon.classList.toggle('fa-toggle-off'); // Change this to the off state class if you have one
+}
