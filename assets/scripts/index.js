f0e4cdc9b04e3249bdc2d536ff6365c8a705b1e7
renzomrtn <martinlorenz.fajardo@gmail.com>
2023-12-24 12:14:27 +0800

    polished the dark mode

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,13 +7,26 @@ function closeNotification() {
 var notification = document.getElementById("notification");
 notification.style.display = "block";
 
-function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
-    
-    // Get the <i> element inside the .toggle element
-    const toggleIcon = document.querySelector('.toggle i');
+// Check if dark mode preference is stored in localStorage
+const isDarkMode = localStorage.getItem('darkMode') === 'true';
 
-    // Toggle the class for the icon
-    toggleIcon.classList.toggle('fa-toggle-on');
-    toggleIcon.classList.toggle('fa-toggle-off'); // Change this to the off state class if you have one
+// Apply dark mode if the preference is true
+    if (isDarkMode) {
+        document.body.classList.add('dark-mode');
+        document.querySelector('.toggle i').classList.add('fa-toggle-on');
+        document.querySelector('.toggle i').classList.remove('fa-toggle-off');
+    }
+
+function toggleDarkMode() {
+// Toggle the dark mode class on the body
+document.body.classList.toggle('dark-mode');
+
+// Toggle the class for the icon
+const toggleIcon = document.querySelector('.toggle i');
+toggleIcon.classList.toggle('fa-toggle-on');
+toggleIcon.classList.toggle('fa-toggle-off');
+
+// Store the dark mode preference in localStorage
+const isDarkModeNow = document.body.classList.contains('dark-mode');
+localStorage.setItem('darkMode', isDarkModeNow.toString());
 }
