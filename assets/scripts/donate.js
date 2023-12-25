1adc0ef821c046f1945eb0bd1d1fd723f06c6b62
renzomrtn <martinlorenz.fajardo@gmail.com>
2023-12-25 16:17:49 +0800

    polished prank

---
+++ assets/scripts/donate.js
@@ -0,0 +1,38 @@
+// Check if dark mode preference is stored in localStorage
+const isDarkMode = localStorage.getItem('darkMode') === 'true';
+
+// Apply dark mode if the preference is true
+if (isDarkMode) {
+    document.body.classList.add('dark-mode');
+    document.querySelector('.toggle i').classList.add('fa-toggle-on');
+    document.querySelector('.toggle i').classList.remove('fa-toggle-off');
+    // Change the image source based on dark mode state
+    const poster = document.getElementById('poster');
+    if (document.body.classList.contains('dark-mode')) {
+        poster.src = 'assets/images/stw.jpg';
+    } else {
+        poster.src = 'assets/images/stw2.jpg';
+    }
+}
+
+function toggleDarkMode() {
+    // Toggle the dark mode class on the body
+    document.body.classList.toggle('dark-mode');
+
+    // Toggle the class for the icon
+    const toggleIcon = document.querySelector('.toggle i');
+    toggleIcon.classList.toggle('fa-toggle-on');
+    toggleIcon.classList.toggle('fa-toggle-off');
+
+    // Toggle the image source based on dark mode state
+    const poster = document.getElementById('poster');
+    if (document.body.classList.contains('dark-mode')) {
+        poster.src = 'assets/images/stw2.jpg';
+    } else {
+        poster.src = 'assets/images/stw.jpg';
+    }
+
+    // Store the dark mode preference in localStorage
+    const isDarkModeNow = document.body.classList.contains('dark-mode');
+    localStorage.setItem('darkMode', isDarkModeNow.toString());
+}
