6e7a2b31c75b56df2e56e0e4a8652abed32579b8
renzomrtn <martinlorenz.fajardo@gmail.com>
2024-01-14 17:04:16 +0800

    fixed modal x bug

--- assets/scripts/works.js
+++ assets/scripts/works.js
@@ -1,3 +1,11 @@
+// Close the modal by clicking anywhere outside the image
+window.onclick = function(event) {
+  var modal = document.getElementById("myModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 // Open the modal and display the clicked image
 function openModal(img) {
   var modal = document.getElementById("myModal");
@@ -5,9 +13,4 @@ function openModal(img) {
   modal.style.display = "flex";
   modal.classList.add("show"); // Add the "show" class to activate the transition
   modalImg.src = img.src;
-}
-// Close the modal
-function closeModal() {
-  var modal = document.getElementById("myModal");
-  modal.style.display = "none";
 }
