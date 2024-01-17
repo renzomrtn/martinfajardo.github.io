86b07b9164b7c03c40c288575f6c1b38e63f85c9
renzomrtn <martinlorenz.fajardo@gmail.com>
2024-01-17 18:18:52 +0800

    image responsiveness

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,3 +6,9 @@ function closeNotification() {
 // Show the notification initially
 var notification = document.getElementById("notification");
 notification.style.display = "block";
+
+window.addEventListener('resize', function () {
+    var image = document.getElementById('landscape-image');
+    var newSrc = window.innerWidth <= 600 ? 'assets/images/homeImg2.jpg' : 'assets/images/homeImg.jpg';
+    image.src = newSrc;
+  });
