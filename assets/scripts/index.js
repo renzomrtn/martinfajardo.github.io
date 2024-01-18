407f295c70ed38e1abb6b025e968077719384769
renzomrtn <martinlorenz.fajardo@gmail.com>
2024-01-18 17:46:15 +0800

    turned donate into its own section

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,6 +9,6 @@ notification.style.display = "block";
 
 window.addEventListener('resize', function () {
     var image = document.getElementById('landscape-image');
-    var newSrc = window.innerWidth <= 600 ? 'assets/images/homeImg2.jpg' : 'assets/images/homeImg.jpg';
+    var newSrc = window.innerWidth <= 700 ? 'assets/images/homeImg2.jpg' : 'assets/images/homeImg.jpg';
     image.src = newSrc;
   });
