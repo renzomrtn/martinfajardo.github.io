75e7f99348b963ca0262208a18f8626a636c32d9
renzomrtn <martinlorenz.fajardo@gmail.com>
2024-01-28 17:38:00 +0800

    homepade responsive img

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -8,9 +8,7 @@ var notification = document.getElementById("notification");
 notification.style.display = "block";
 
 window.addEventListener('resize', function () {
-    console.log("Window resized");
     var image = document.getElementById('landscape-image');
-    var newSrc = window.innerWidth <= 1000 ? 'assets/images/home/homeImg2.jpg?cache=' + Date.now() : 'assets/images/home/homeImg.jpg?cache=' + Date.now();
-    console.log("New source: " + newSrc);
+    var newSrc = window.innerWidth <= 1000 ? 'assets/images/home/homeImg2.jpg' : 'assets/images/home/homeImg.jpg';
     image.src = newSrc;
 });
