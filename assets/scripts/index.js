6e6d9a1ddb80821f4bd31c74b4f5ad2c503bbd60
renzomrtn <martinlorenz.fajardo@gmail.com>
2024-01-28 17:32:45 +0800

    homepage responsive experiment

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -8,7 +8,9 @@ var notification = document.getElementById("notification");
 notification.style.display = "block";
 
 window.addEventListener('resize', function () {
+    console.log("Window resized");
     var image = document.getElementById('landscape-image');
-    var newSrc = window.innerWidth <= 1000 ? 'assets/images/home/homeImg2.jpg' : 'assets/images/home/homeImg.jpg';
+    var newSrc = window.innerWidth <= 1000 ? 'assets/images/home/homeImg2.jpg?cache=' + Date.now() : 'assets/images/home/homeImg.jpg?cache=' + Date.now();
+    console.log("New source: " + newSrc);
     image.src = newSrc;
 });
