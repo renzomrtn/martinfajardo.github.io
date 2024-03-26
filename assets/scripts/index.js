3a273e4bb571dbe2b367a81fad5870490ac370ad
renzomrtn <martinlorenz.fajardo@gmail.com>
2024-03-26 17:56:08 +0800

    updated js image size bug

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,8 +7,22 @@ function closeNotification() {
 var notification = document.getElementById("notification");
 notification.style.display = "block";
 
+/*
 window.addEventListener('resize', function () {
     var image = document.getElementById('landscape-image');
     var newSrc = window.innerWidth <= 1000 ? 'assets/images/home/homeImg2.jpg' : 'assets/images/home/homeImg.jpg';
     image.src = newSrc;
 });
+*/
+// Function to handle image resizing
+function handleImageResize() {
+    var image = document.getElementById('landscape-image');
+    var newSrc = window.innerWidth <= 1000 ? 'assets/images/home/homeImg2.jpg' : 'assets/images/home/homeImg.jpg';
+    image.src = newSrc;
+}
+
+// Call the function when the page loads
+window.addEventListener('load', handleImageResize);
+
+// Call the function when the window is resized
+window.addEventListener('resize', handleImageResize);
