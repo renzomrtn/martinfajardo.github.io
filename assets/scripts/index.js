35ef2664b8b1769942f6069d098495659a0b432b
renzomrtn <martinlorenz.fajardo@gmail.com>
2024-04-14 13:59:35 +0800

    removed unnecessary js for homepage img resize

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,13 +7,6 @@ function closeNotification() {
 var notification = document.getElementById("notification");
 notification.style.display = "block";
 
-/*
-window.addEventListener('resize', function () {
-    var image = document.getElementById('landscape-image');
-    var newSrc = window.innerWidth <= 1000 ? 'assets/images/home/homeImg2.jpg' : 'assets/images/home/homeImg.jpg';
-    image.src = newSrc;
-});
-*/
 // Function to handle image resizing
 function handleImageResize() {
     var image = document.getElementById('landscape-image');
