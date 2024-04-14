d689dd150af9dd5857f31e81409c7ed1362a323b
renzomrtn <martinlorenz.fajardo@gmail.com>
2024-04-14 11:47:36 +0800

    added js for changing .name translation

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -26,3 +26,21 @@ window.addEventListener('load', handleImageResize);
 
 // Call the function when the window is resized
 window.addEventListener('resize', handleImageResize);
+
+// For name hover
+var texts = ["Soy Martin", "Ako si Martin", "Sono Martin", "я Мартин", "私はマーティンです", "I'm Martin"];
+var currentIndex = 0;
+var originalText = document.querySelector('.name').textContent;
+
+document.querySelector('.name').addEventListener('mouseover', function() {
+    var hoverText = this;
+    var interval = setInterval(function() {
+      hoverText.textContent = texts[currentIndex];
+      currentIndex = (currentIndex + 1) % texts.length;
+    }, 2000); // Change text every 2 seconds
+
+    hoverText.addEventListener('mouseout', function() {
+      clearInterval(interval);
+      hoverText.textContent = originalText; // Reset text to original when mouse leaves
+    });
+});
