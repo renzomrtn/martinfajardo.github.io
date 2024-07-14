a25ef143d8198c800fe20341f81e5bf3f4ecd78e
Martin Lorenz Fajardo <martinlorenz.fajardo@gmail.com>
2024-07-14 14:45:25 +0800

    major change i'm too lazy to explain

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,17 +1,3 @@
-// Function to handle image resizing
-function handleImageResize() {
-    var image = document.getElementById('landscape-image');
-    var newSrc = window.innerWidth <= 1000 ? 'assets/images/home/homeImg2.jpg' : 'assets/images/home/homeImg.jpg';
-    image.src = newSrc;
-}
-
-// Call the function when the page loads
-window.addEventListener('load', handleImageResize);
-
-// Call the function when the window is resized
-window.addEventListener('resize', handleImageResize);
-
-
 // For name hover
 var texts = ["Soy Martin", "Ako si Martin", "Sono Martin", "я Мартин", "私はマーティンです", "I'm Martin"];
 var currentIndex = 0;
@@ -22,7 +8,7 @@ document.querySelector('.name').addEventListener('mouseover', function() {
   var interval = setInterval(function() {
     hoverText.textContent = texts[currentIndex];
     currentIndex = (currentIndex + 1) % texts.length;
-  }, 2000); // Change text every 2 seconds
+  }, 1500);
 
   hoverText.addEventListener('mouseout', function() {
     clearInterval(interval);
